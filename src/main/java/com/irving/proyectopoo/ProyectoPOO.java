464497f7e1513ebd1b4bdaf925f65a6ce18f1a54
EdwinAPG123 <edwinperezgarcia@gmail.com>
2025-04-01 00:53:19 -0600

    este es nuestro proyecto de poo Signed-off-by:EdwinAPG123 <edwinperezgarcia777@gmail.com>

---
+++ src/main/java/com/irving/proyectopoo/ProyectoPOO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.irving.proyectopoo;
+
+/**
+ *
+ * @author eciap
+ */
+public class ProyectoPOO {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
